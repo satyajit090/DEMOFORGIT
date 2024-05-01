d50e9861f8b0a02210c9e26d32d2c0f89cde18f3
Satyajit <beherasatyajit090@gmail.com>
2024-05-01 12:03:57 +0530

    for demo purpose

---
+++ src/test/java/com/example/demo/DemoforgitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoforgitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
